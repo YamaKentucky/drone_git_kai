009c9e211a2cafa336a6eda03b79522b83692bff
doraiden555 <doraiden555777@yahoo.co.jp>
2019-11-15 18:38:09 +0900

    add DD_e[i] for delete the outlier of UWB

--- pos_test_UWB_opt-01.py
+++ pos_test_UWB_opt-01.py
@@ -295,22 +295,24 @@ def pos_estimate(bias_x = 0,bias_y = 0,bias_z = 0):
 
     omega = np.array([(m9g[0]-bias_gyro_x),(m9g[1]-bias_gyro_y),m9g[2]-bias_gyro_z])
 
+    for i in range 4:
+        DD_e[i] = DD[i] 
 
-    if abs(int(DD_old[0]) - int(DD[0])) < 120:
-        dd1 = int(DD[0])*0.01   ## mm -> m
-        DD_old[0] = DD[0]
+    if abs(int(DD_old[0]) - int(DD_e[0])) < 120:
+        dd1 = int(DD_e[0])*0.01   ## mm -> m
+        DD_old[0] = DD_e[0]
 
-    if abs(int(DD_old[1]) - int(DD[1])) < 120:
-        dd2 = int(DD[1])*0.01
-        DD_old[1] = DD[1]
+    if abs(int(DD_old[1]) - int(DD_e[1])) < 120:
+        dd2 = int(DD_e[1])*0.01
+        DD_old[1] = DD_e[1]
 
-    if abs(int(DD_old[2]) - int(DD[2])) < 120:
-        dd3 = int(DD[2])*0.01
-        DD_old[2] = DD[2]
+    if abs(int(DD_old[2]) - int(DD_e[2])) < 120:
+        dd3 = int(DD_e[2])*0.01
+        DD_old[2] = DD_e[2]
         
-    if abs(int(DD_old[3]) - int(DD[3])) < 120:
-        dd4 = int(DD[3])*0.01
-        DD_old[3] = DD[3]
+    if abs(int(DD_old[3]) - int(DD_e[3])) < 120:
+        dd4 = int(DD_e[3])*0.01
+        DD_old[3] = DD_e[3]
 
     v_xopt = deltaX * c_psi - deltaY * s_psi
     v_yopt = deltaX * s_psi + deltaY * c_psi
